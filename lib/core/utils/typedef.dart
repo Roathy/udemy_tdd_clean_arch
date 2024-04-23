4bd6e007950ef74b98a3381518ce3924a87dbc12
Julio Vergara <gogczoro@gmail.com>
2024-04-23 16:54:09 -0600

    Add DataMap typedef

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -1,8 +1,12 @@
 import 'package:dartz/dartz.dart';
-import 'package:tdd_clean_arch/core/errors/failure.dart';
+
+import '../errors/failure.dart';
 
 /// Represents a Future that returns an Either of a [Failure] or a generic type [T].
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 
 /// Represents a Future that returns an Either of a [Failure] or `void`.
 typedef ResultVoid = Future<Either<Failure, void>>;
+
+/// Represents a
+typedef DataMap = Map<String, dynamic>;
