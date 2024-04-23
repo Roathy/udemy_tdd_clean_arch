fa00ded0b0d19d9adf63318a72fc5caa3a9a5e8d
Julio Vergara <gogczoro@gmail.com>
2024-04-23 16:58:27 -0600

    Fix var name

--- lib/src/features/authentication/domain/usecases/create_user.dart
+++ lib/src/features/authentication/domain/usecases/create_user.dart
@@ -1,7 +1,8 @@
 import 'package:equatable/equatable.dart';
-import 'package:tdd_clean_arch/core/usecase/usecase.dart';
-import 'package:tdd_clean_arch/core/utils/typedef.dart';
-import 'package:tdd_clean_arch/src/features/authentication/domain/repositories/authentication_repository.dart';
+
+import '../../../../../core/usecase/usecase.dart';
+import '../../../../../core/utils/typedef.dart';
+import '../repositories/authentication_repository.dart';
 
 class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
   const CreateUser(this._repository);
@@ -11,33 +12,33 @@ class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
   @override
   ResultVoid call(CreateUserParams params) async => _repository.createUser(
         name: params.name,
+        avatar: params.avatar,
         createdAt: params.createdAt,
-        avatarUrl: params.avatarUrl,
       );
 }
 
 class CreateUserParams extends Equatable {
   const CreateUserParams({
     required this.name,
+    required this.avatar,
     required this.createdAt,
-    required this.avatarUrl,
   });
 
   const CreateUserParams.empty()
       : this(
           name: '_empty.name',
+          avatar: '_empty.avatar',
           createdAt: '_empty.createdAt',
-          avatarUrl: '_empty.avatarUrl',
         );
 
   final String name;
+  final String avatar;
   final String createdAt;
-  final String avatarUrl;
 
   @override
   List<Object?> get props => [
         name,
+        avatar,
         createdAt,
-        avatarUrl
       ];
 }
