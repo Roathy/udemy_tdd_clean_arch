c37bf8b84fd7d953006b9a9aa341e91092217888
Julio Vergara <gogczoro@gmail.com>
2024-04-29 13:15:37 -0600

    fix imports

--- lib/src/features/authentication/presentation/bloc/authentication_bloc.dart
+++ lib/src/features/authentication/presentation/bloc/authentication_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:tdd_clean_arch/src/features/authentication/domain/entities/user.dart';
 
+import '../../domain/entities/user.dart';
 import '../../domain/usecases/create_user.dart';
 import '../../domain/usecases/get_users.dart';
 
