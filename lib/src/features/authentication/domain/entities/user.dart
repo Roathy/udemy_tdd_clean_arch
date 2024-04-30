9590aa045106d17879d2759eae85cbb3c217fdf3
Julio Vergara <gogczoro@gmail.com>
2024-04-30 16:51:44 -0600

    remove documentation

--- lib/src/features/authentication/domain/entities/user.dart
+++ lib/src/features/authentication/domain/entities/user.dart
@@ -4,14 +4,7 @@ import 'package:equatable/equatable.dart';
 // the different layers of the particular feature and from here
 // we go to the repository
 
-/// Represents a user entity in the authentication domain.
-///
-/// The `User` class holds the essential information about a user, including their name,
-/// avatar URL, creation timestamp, and a unique identifier. This entity is used
-/// throughout the different layers of the authentication feature, serving as the
-/// blueprint for user data.
 class User extends Equatable {
-  /// Creates a new instance of the `User` class with the provided properties.
   const User({
     required this.id,
     required this.name,
