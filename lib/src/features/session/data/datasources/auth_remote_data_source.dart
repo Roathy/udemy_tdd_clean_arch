c3a76e2e731df0b3a849ab5695fc4d7f18a2a3af
Julio Vergara <gogczoro@gmail.com>
2024-05-02 14:44:30 -0600

    fix endpoing - add header

--- lib/src/features/session/data/datasources/auth_remote_data_source.dart
+++ lib/src/features/session/data/datasources/auth_remote_data_source.dart
@@ -1,25 +1,26 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:tdd_clean_arch/src/features/session/data/models/api_response_model.dart';
 
 import '../../../../../core/errors/exception.dart';
 import '../../../../../core/utils/constants.dart';
 
 abstract class AuthRemoteDataSource {
-  Future<void> login({
+  Future<Object> login({
     required email,
     required password,
   });
 }
 
-const loginEndPoint = '/alumno/login';
+const loginEndPoint = '/api/v1/alumno/login';
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   const AuthRemoteDataSourceImpl(this._client);
   final http.Client _client;
 
   @override
-  Future<void> login({
+  Future<Object> login({
     required email,
     required password,
   }) async {
@@ -30,7 +31,9 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
             'password': password,
           }),
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-App-MirHorizon': '2c87d4de6231dee7d4557627c3812497'
           });
       if (response.statusCode != 200 && response.statusCode != 201) {
         throw ApiException(
@@ -38,6 +41,8 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
           statusCode: response.statusCode,
         );
       }
+
+      return ApiResponseModel.fromMap(jsonDecode(response.body));
     } on ApiException {
       rethrow;
     } catch (e) {
