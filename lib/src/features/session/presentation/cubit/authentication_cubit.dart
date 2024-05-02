c878436a22b7349db9cb4004724a0a6a5526d53d
Julio Vergara <gogczoro@gmail.com>
2024-05-02 14:46:28 -0600

    setup authentication cubit

--- lib/src/features/session/presentation/cubit/authentication_cubit.dart
+++ lib/src/features/session/presentation/cubit/authentication_cubit.dart
@@ -1,8 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:tdd_clean_arch/src/features/session/data/models/api_response_model.dart';
+
+import '../../domain/usecases/login.dart';
 
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
-  AuthenticationCubit() : super(AuthenticationInitial());
+  AuthenticationCubit({
+    required Login login,
+  })  : _login = login,
+        super(const AuthenticationInitial());
+
+  final Login _login;
+
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    emit(const CreatingUser());
+
+    final result = await _login(LoginCredentialsParams(
+      email: email,
+      password: password,
+    ));
+
+    result.fold(
+      (failure) => emit(AuthenticationError(failure.errorMessage)),
+      (response) => emit(UsersLoaded(response)),
+    );
+  }
 }
--- lib/src/features/session/presentation/cubit/authentication_state.dart
+++ lib/src/features/session/presentation/cubit/authentication_state.dart
@@ -1,10 +1,44 @@
 part of 'authentication_cubit.dart';
 
-sealed class AuthenticationState extends Equatable {
+abstract class AuthenticationState extends Equatable {
   const AuthenticationState();
 
   @override
   List<Object> get props => [];
 }
 
-final class AuthenticationInitial extends AuthenticationState {}
+class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this.response);
+
+  final ApiResponseModel response;
+
+  // @override
+  // List<Object> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<String> get props => [
+        message
+      ];
+}
