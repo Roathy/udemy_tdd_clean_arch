b740df4937c0b93276163dc3c7557c4b555bee91
Julio Vergara <gogczoro@gmail.com>
2024-05-02 14:43:20 -0600

    inject session/login

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,12 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
-import '../../src/features/authentication/data/datasources/authentication_remote_data_source.dart';
-import '../../src/features/authentication/data/repositories/authentication_repository_impl.dart';
-import '../../src/features/authentication/domain/repositories/authentication_repository.dart';
-import '../../src/features/authentication/domain/usecases/create_user.dart';
-import '../../src/features/authentication/domain/usecases/get_users.dart';
-import '../../src/features/authentication/presentation/cubit/authentication_cubit.dart';
+import '../../src/features/session/data/datasources/auth_remote_data_source.dart';
+import '../../src/features/session/data/repositories/auth_repo_impl.dart';
+import '../../src/features/session/domain/repositories/auth_repo.dart';
+import '../../src/features/session/domain/usecases/login.dart';
+import '../../src/features/session/presentation/cubit/authentication_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -14,16 +13,43 @@ Future<void> init() async {
   sl
     // app logic
     ..registerFactory(() => AuthenticationCubit(
-          createUser: sl(),
-          getUsers: sl(),
+          login: sl(),
         ))
     // Usecases
-    ..registerLazySingleton(() => CreateUser(sl()))
-    ..registerLazySingleton(() => GetUsers(sl()))
+    ..registerLazySingleton(() => Login(sl()))
     // Repositories
-    ..registerLazySingleton<AuthenticationRepository>(() => AuthenticationRepositoryImpl(sl()))
+    ..registerLazySingleton<AuthRepo>(() => AuthRepoImpl(sl()))
     // DataSource
-    ..registerLazySingleton<AuthenticationRemoteDataSource>(() => AuthRemoteDataSourceImpl(sl()))
+    ..registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSourceImpl(sl()))
     // External Dependencies
     ..registerLazySingleton(http.Client.new);
 }
+// import 'package:get_it/get_it.dart';
+// import 'package:http/http.dart' as http;
+
+// import '../../src/features/authentication/data/datasources/authentication_remote_data_source.dart';
+// import '../../src/features/authentication/data/repositories/authentication_repository_impl.dart';
+// import '../../src/features/authentication/domain/repositories/authentication_repository.dart';
+// import '../../src/features/authentication/domain/usecases/create_user.dart';
+// import '../../src/features/authentication/domain/usecases/get_users.dart';
+// import '../../src/features/authentication/presentation/cubit/authentication_cubit.dart';
+
+// final sl = GetIt.instance;
+
+// Future<void> init() async {
+//   sl
+//     // app logic
+//     ..registerFactory(() => AuthenticationCubit(
+//           createUser: sl(),
+//           getUsers: sl(),
+//         ))
+//     // Usecases
+//     ..registerLazySingleton(() => CreateUser(sl()))
+//     ..registerLazySingleton(() => GetUsers(sl()))
+//     // Repositories
+//     ..registerLazySingleton<AuthenticationRepository>(() => AuthenticationRepositoryImpl(sl()))
+//     // DataSource
+//     ..registerLazySingleton<AuthenticationRemoteDataSource>(() => AuthRemoteDataSourceImpl(sl()))
+//     // External Dependencies
+//     ..registerLazySingleton(http.Client.new);
+// }
