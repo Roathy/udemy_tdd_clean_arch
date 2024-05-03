ce0b2554f75899e08286bf14b6f68bdf4c730429
Julio Vergara <gogczoro@gmail.com>
2024-05-03 14:38:28 -0600

    register dependencies - update data sources impl

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,11 +1,15 @@
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
+import '../../src/features/session/data/datasources/auth_local_data_source.dart';
 import '../../src/features/session/data/datasources/auth_remote_data_source.dart';
 import '../../src/features/session/data/repositories/auth_repo_impl.dart';
 import '../../src/features/session/domain/repositories/auth_repo.dart';
 import '../../src/features/session/domain/usecases/login.dart';
 import '../../src/features/session/presentation/cubit/authentication_cubit.dart';
+import '../newtwork/network_info.dart';
+import 'shared_preferences_service.dart';
 
 final sl = GetIt.instance;
 
@@ -18,23 +22,28 @@ Future<void> init() async {
     // Usecases
     ..registerLazySingleton(() => Login(sl()))
     // Repositories
-    ..registerLazySingleton<AuthRepo>(() => AuthRepoImpl(sl()))
+    ..registerLazySingleton<AuthRepo>(() {
+      return AuthRepoImpl(
+        localDataSource: sl<AuthLocalDataSource>(),
+        remoteDataSource: sl<AuthRemoteDataSource>(),
+        networkInfo: sl<NetworkInfo>(),
+      );
+    })
     // DataSource
     ..registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSourceImpl(sl()))
+    ..registerLazySingleton<AuthLocalDataSource>(() => AuthLocalDataSourceImpl(sl<SharedPreferencesService>()))
     // External Dependencies
     ..registerLazySingleton(http.Client.new);
+
+  // Register SharedPreferencesService
+  sl.registerLazySingleton(() => SharedPreferencesService());
+
+  // Register NetworkInfoImp with DataConnectionChecker
+  sl.registerFactory<NetworkInfo>(() {
+    final connectionChecker = DataConnectionChecker();
+    return NetworkInfoImp(connectionChecker);
+  });
 }
-// import 'package:get_it/get_it.dart';
-// import 'package:http/http.dart' as http;
-
-// import '../../src/features/authentication/data/datasources/authentication_remote_data_source.dart';
-// import '../../src/features/authentication/data/repositories/authentication_repository_impl.dart';
-// import '../../src/features/authentication/domain/repositories/authentication_repository.dart';
-// import '../../src/features/authentication/domain/usecases/create_user.dart';
-// import '../../src/features/authentication/domain/usecases/get_users.dart';
-// import '../../src/features/authentication/presentation/cubit/authentication_cubit.dart';
-
-// final sl = GetIt.instance;
 
 // Future<void> init() async {
 //   sl
