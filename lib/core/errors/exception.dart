c1b4b1c92404df92fb28c0d1c4dee17ffd90b9f2
Julio Vergara <gogczoro@gmail.com>
2024-05-03 14:36:44 -0600

    impl cache exception

--- lib/core/errors/exception.dart
+++ lib/core/errors/exception.dart
@@ -1,3 +1,5 @@
+// The repository will catch these exceptions and convert them into Failure, therefore Failures map one to one to Exceptions
+
 import 'package:equatable/equatable.dart';
 
 class ApiException extends Equatable implements Exception {
@@ -12,3 +14,20 @@ class ApiException extends Equatable implements Exception {
         statusCode
       ];
 }
+
+// Will be thrown from between the local data source when a cache error happens
+class CacheException extends Equatable implements Exception {
+  const CacheException({required this.message, required this.statusCode});
+
+  final int statusCode;
+  final String message;
+
+  @override
+  List<Object?> get props => [
+        message,
+        statusCode
+      ];
+}
+
+// 404
+class ServerException implements Exception {}
